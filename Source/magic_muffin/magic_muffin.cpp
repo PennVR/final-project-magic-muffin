f34248c4c54dd489d02c47a523f3b41ad65972cd
Jon Lee <agentlee927@gmail.com>
2017-05-01 14:04:53 -0400

    final working version ha

---
+++ Source/magic_muffin/magic_muffin.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "magic_muffin.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, magic_muffin, "magic_muffin" );
